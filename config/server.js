6bf2bbfcaad48c0b7cc8b6b1957d050a7aedfbb7
ChrisKnowsDev <chrisharley81@gmail.com>
2021-03-08 21:57:40 -0500

    update database config

--- config/server.js
+++ config/server.js
@@ -1,9 +1,4 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'c628dd417e9265b477a91ac8b76837fd'),
-    },
-  },
+  url: env('HEROKU_URL'),
 });
+
